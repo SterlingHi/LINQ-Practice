149bf377986f8a0934e398b8a5c47dc44c89fc69
SterlingHi <sterlinghisey@yahoo.com>
2021-08-09 10:05:05 -0500

    problems 3 & 4 complete

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -62,14 +62,22 @@ namespace DatabaseFirstLINQ
         {
             // Write a LINQ query that gets each product where the products price is greater than $150.
             // Then print the name and price of each product from the above query to the console.
-
+            var expensiveProduct = _context.Products.Where(p => p.Price >= 150);
+            foreach (var product in expensiveProduct)
+            {
+                Console.WriteLine(product.Name + " $" + product.Price);
+            }
         }
 
         private void ProblemFour()
         {
             // Write a LINQ query that gets each product that contains an "s" in the products name.
             // Then print the name of each product from the above query to the console.
-
+            var productsWithS = _context.Products.Where(p => p.Name.Contains("s"));
+            foreach (var product in productsWithS)
+            {
+                Console.WriteLine(product.Name);
+            }
         }
 
         private void ProblemFive()
